69aa8db92af292a82e09d15ae1c9a487f4248998
dotneB <benoit@tioneb.ca>
2011-07-16 17:02:47 -0400

    A way to submit stats

--- Managers/FistBumpStatisticManager.cs
+++ Managers/FistBumpStatisticManager.cs
@@ -16,39 +16,14 @@ public class FistBumpStatisticManager : MonoBehaviour
 
     #endregion
 
-    #region Public Properties
+    #region Public Methods
 
-    #endregion
-
-    #region Implementation of MonoBehaviour
-
-    /// <summary>
-    /// Awake is called when the script instance is being loaded.
-    /// </summary>
-    void Awake()
+    public void Submit(string name, int value)
     {
-
+        FistBumpStatistic stat = m_Statistics.Find(s => s.Name == name);
+        if (stat != null)
+            stat.Submit(value);
     }
-
-    /// <summary>
-    /// Start is called just before any of the Update methods is called the first time.
-    /// </summary>
-    void Start()
-    {
-
-    }
-
-    /// <summary>
-    /// Update is called every frame, if the MonoBehaviour is enabled.
-    /// </summary>
-    void Update()
-    {
-        // Update is called once per frame
-    }
-
-    #endregion
-
-    #region Private Methods
 
     #endregion
 }
