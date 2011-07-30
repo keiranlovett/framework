62b2e04c4c9f6a7d4ba7842bdfdd325a20ab0533
dotneB <benoit@tioneb.ca>
2011-07-30 11:42:25 -0400

    - Bug fix: default value for firstUse should be true - New Feature: Serialization/Deserialization of the stats

--- FistBumpStatistic.cs
+++ FistBumpStatistic.cs
@@ -1,12 +1,24 @@
 ﻿#region Using statements
+
+using System.Collections.Generic;
 using UnityEngine;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+using System;
+using System.Runtime.Serialization;
+using System.Reflection;
+
 #endregion
 
 /// <summary>
 /// 
 /// </summary>
 /// <remarks>FistBump.ca - Copyright (C)</remarks>
-public class FistBumpStatistic
+[Serializable()]
+public class FistBumpStatistic : ISerializable
 {
     #region Enums
     public enum StatisticType
@@ -20,7 +32,7 @@ public class FistBumpStatistic
 
     #region Private Fields
 
-    private bool m_FirstUse = false;
+    private bool m_FirstUse = true;
     private string m_Name = "";
     private StatisticType m_Type = StatisticType.Add;
     private int m_CurrentValue = 0;
@@ -78,6 +90,24 @@ public class FistBumpStatistic
     }
 
     #endregion
+
+    public FistBumpStatistic(SerializationInfo info, StreamingContext ctxt)
+    {
+        m_FirstUse = (bool)info.GetValue("firstuse", typeof(bool)); ;
+        m_Name = (string)info.GetValue("name", typeof(string)); ;
+        m_Type = (StatisticType)info.GetValue("type", typeof(StatisticType)); ;
+        m_CurrentValue = (int)info.GetValue("currentvalue", typeof(int));
+    }
+
+    //Serialization function.
+
+    public void GetObjectData(SerializationInfo info, StreamingContext ctxt)
+    {
+        info.AddValue("firstuse", m_FirstUse); ;
+        info.AddValue("name", m_Name); ;
+        info.AddValue("type", m_Type); ;
+        info.AddValue("currentvalue", m_CurrentValue);
+    }
 }
 
 public partial class StatisticName
--- Managers/FistBumpStatisticManager.cs
+++ Managers/FistBumpStatisticManager.cs
@@ -2,6 +2,14 @@
 
 using System.Collections.Generic;
 using UnityEngine;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+using System;
+using System.Runtime.Serialization;
+using System.Reflection;
 #endregion
 
 /// <summary>
@@ -25,5 +33,30 @@ public class FistBumpStatisticManager : MonoBehaviour
             stat.Submit(value);
     }
 
+    public void Serialize(SerializationInfo info)
+    {
+        m_Statistics = (List<FistBumpStatistic>)info.GetValue("stats", typeof(List<FistBumpStatistic>));
+    }
+
+    public void Deserialize(SerializationInfo info)
+    {
+        info.AddValue("stats", m_Statistics);
+    }
+
+    public void Add(string name, FistBumpStatistic.StatisticType type)
+    {
+        m_Statistics.Add(new FistBumpStatistic(name, type));
+    }
+
+    #endregion
+
+    #region Implementation of MonoBehaviour
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    public void Awake()
+    {
+        m_Statistics = new List<FistBumpStatistic>();
+    }
     #endregion
 }
