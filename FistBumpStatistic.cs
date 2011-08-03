c84e11f5e8c39b6b6c0df1a636ae0c02a067bc29
dotneB <benoit@tioneb.ca>
2011-08-02 22:02:33 -0400

    Split the Stats in 2, the Definition and the Stat. As long as a stat isn't used, it should only exist as a definition, not as an actual stat

--- FistBumpStatistic.cs
+++ FistBumpStatistic.cs
@@ -13,82 +13,38 @@ using System.Runtime.Serialization;
 [Serializable()]
 public class FistBumpStatistic : ISerializable
 {
-    #region Enums
-    public enum StatisticType
-    {
-        Min,
-        Max,
-        Add,
-        Replace
-    }
-    #endregion
-
     #region Private Fields
 
     private readonly int m_Name = 0;
-    private readonly StatisticType m_Type = StatisticType.Add;
     private int m_CurrentValue = 0;
 
     #endregion
 
     #region Public Fields
 
-    public bool             HasBeenUsed     { get { return (m_Type == StatisticType.Min ? m_CurrentValue != int.MaxValue : m_CurrentValue != 0); } }
     public int              Name            { get { return m_Name; } }
-    public StatisticType    Type            { get { return m_Type; } }
-    public int              Value           { get { return m_CurrentValue; } }
+    public int              Value           { get { return m_CurrentValue; } set { m_CurrentValue = value; }}
 
     #endregion
 
     #region Ctor
 
-    public FistBumpStatistic(int name, StatisticType type)
+    public FistBumpStatistic(int name, FistBumpStatisticManager.StatisticType type)
     {
         m_Name = name;
-        m_Type = type;
-        if (type == StatisticType.Min)
+        if (type == FistBumpStatisticManager.StatisticType.Min)
             m_CurrentValue = int.MaxValue;
     }
 
     public FistBumpStatistic(SerializationInfo info, StreamingContext ctxt)
     {
         m_Name = (int)info.GetValue("name", typeof(int));
-        m_Type = (StatisticType)info.GetValue("type", typeof(StatisticType));
         m_CurrentValue = (int)info.GetValue("currentvalue", typeof(int));
     }
 
     #endregion
 
     #region Public Methods
-    
-    public void Submit(int value)
-    {
-        switch(m_Type)
-        {
-            case StatisticType.Add:
-                Debug.Log(string.Format("[Stats] {0} - New Total - Old={1} New={2}", m_Name, m_CurrentValue, m_CurrentValue + value));
-                m_CurrentValue += value;
-                break;
-            case StatisticType.Min:
-                if (value < m_CurrentValue)
-                {
-                    Debug.Log(string.Format("[Stats] {0} - New Min - {1}New={2}", m_Name, (HasBeenUsed ? string.Format("Old={0} ", m_CurrentValue) : ""), value));
-                    m_CurrentValue = value;
-                }
-                break;
-            case StatisticType.Max:
-                if (value > m_CurrentValue)
-                {
-                    Debug.Log(string.Format("[Stats] {0} - New Max - {1}New={2}", m_Name, (HasBeenUsed ? string.Format("Old={0} ", m_CurrentValue) : ""), value));
-                    m_CurrentValue = value;
-                }
-                break;
-            case StatisticType.Replace:
-                Debug.Log(string.Format("[Stats] {0} - New Value - {1}New={2}", m_Name, (HasBeenUsed ? string.Format("Old={0} ", m_CurrentValue) : ""), value));
-                m_CurrentValue = value;
-                break;
-        }
-    }
 
     #endregion
 
@@ -97,14 +53,8 @@ public class FistBumpStatistic : ISerializable
     public void GetObjectData(SerializationInfo info, StreamingContext context)
     {
         info.AddValue("name", m_Name);
-        info.AddValue("type", m_Type);
         info.AddValue("currentvalue", m_CurrentValue);
     }
 
     #endregion
-}
-
-public partial class StatisticName
-{
-
 }
--- Managers/FistBumpStatisticManager.cs
+++ Managers/FistBumpStatisticManager.cs
@@ -12,9 +12,35 @@ using System.Runtime.Serialization;
 /// <remarks>FistBump.ca - Copyright (C)</remarks>
 public class FistBumpStatisticManager : MonoBehaviour
 {
+    #region Enums
+    public enum StatisticType
+    {
+        Min,
+        Max,
+        Add,
+        Replace
+    }
+    #endregion
+
+    private class FistBumpStatisticDefinition
+    {
+        private readonly int m_Name = 0;
+        private readonly StatisticType m_Type = StatisticType.Add;
+
+        public int Name { get { return m_Name; } }
+        public StatisticType Type { get { return m_Type; } }
+
+        public FistBumpStatisticDefinition(int name, StatisticType type)
+        {
+            m_Name = name;
+            m_Type = type;
+        }
+    }
+
     #region Private Fields
 
     private List<FistBumpStatistic> m_Statistics = new List<FistBumpStatistic>();
+    private readonly List<FistBumpStatisticDefinition> m_StatisticsDefinitions = new List<FistBumpStatisticDefinition>();
 
     #endregion
 
@@ -22,9 +48,45 @@ public class FistBumpStatisticManager : MonoBehaviour
 
     public void Submit(int statName, int statValue)
     {
+        FistBumpStatisticDefinition statDef = m_StatisticsDefinitions.Find(s => s.Name == statName);
         FistBumpStatistic stat = m_Statistics.Find(s => s.Name == statName);
-        if (stat != null)
-            stat.Submit(statValue);
+        if (statDef == null)
+        {
+            Debug.LogWarning(string.Format("[Stats] Stat definition {0} does not exist, you must add the stat before submitting it", statName));
+            return;
+        }
+
+        bool newStat = stat == null;
+        if (newStat)
+        {
+            stat = new FistBumpStatistic(statName, statDef.Type);
+            m_Statistics.Add(stat);
+        }
+        switch (statDef.Type)
+        {
+            case StatisticType.Add:
+                Debug.Log(string.Format("[Stats] {0} - New Total - Old={1} New={2}", stat.Name, stat.Value, stat.Value + statValue));
+                stat.Value += statValue;
+                break;
+            case StatisticType.Min:
+                if (statValue < stat.Value)
+                {
+                    Debug.Log(string.Format("[Stats] {0} - New Min - {1}New={2}", stat.Name, (!newStat ? string.Format("Old={0} ", stat.Value) : ""), statValue));
+                    stat.Value = statValue;
+                }
+                break;
+            case StatisticType.Max:
+                if (statValue > stat.Value)
+                {
+                    Debug.Log(string.Format("[Stats] {0} - New Max - {1}New={2}", stat.Name, (!newStat ? string.Format("Old={0} ", stat.Value) : ""), statValue));
+                    stat.Value = statValue;
+                }
+                break;
+            case StatisticType.Replace:
+                Debug.Log(string.Format("[Stats] {0} - New Value - {1}New={2}", stat.Name, (!newStat ? string.Format("Old={0} ", stat.Value) : ""), statValue));
+                stat.Value = statValue;
+                break;
+        }
     }
 
     public void Deserialize(SerializationInfo info)
@@ -37,10 +99,10 @@ public class FistBumpStatisticManager : MonoBehaviour
         info.AddValue("stats", m_Statistics);
     }
 
-    public void Add(int statName, FistBumpStatistic.StatisticType type)
+    public void Add(int statName, StatisticType type)
     {
-        if (m_Statistics.Find(s => s.Name == statName) == null)
-            m_Statistics.Add(new FistBumpStatistic(statName, type));
+        if (m_StatisticsDefinitions.Find(s => s.Name == statName) == null)
+            m_StatisticsDefinitions.Add(new FistBumpStatisticDefinition(statName, type));
     }
 
     #endregion
@@ -50,4 +112,9 @@ public class FistBumpStatisticManager : MonoBehaviour
 
 
     #endregion
+}
+
+public partial class StatisticName
+{
+
 }
