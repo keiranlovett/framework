77bdb6f872a1e14a7b345c642f55cdc285c07f15
dotneB <benoit@tioneb.ca>
2012-10-05 19:06:18 -0400

    Renamed Set* to SetPosition*

--- Utils/Extensions.cs
+++ Utils/Extensions.cs
@@ -15,17 +15,17 @@ namespace FistBump.Framework.ExtensionMethods
         #endregion
 
         #region Transforms Extensions
-        public static void SetX(this Transform transform, float x)
+        public static void SetPositionX(this Transform transform, float x)
         {
             transform.position = new Vector3(x, transform.position.y, transform.position.z);
         }
 
-        public static void SetY(this Transform transform, float y)
+        public static void SetPositionY(this Transform transform, float y)
         {
             transform.position = new Vector3(transform.position.x, y, transform.position.z);
         }
 
-        public static void SetZ(this Transform transform, float z)
+        public static void SetPositionZ(this Transform transform, float z)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, z);
         }
