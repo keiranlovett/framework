03625be6f2e22451ccf2aff5010de2ddcd91db50
dotneB <benoit@tioneb.ca>
2012-11-01 00:42:03 -0400

    Some more cleanup

--- Social/SocialPlatformSelector.cs
+++ Social/SocialPlatformSelector.cs
@@ -9,29 +9,26 @@ namespace FistBump.Framework
     public static class SocialPlatformSelector
     {
         private static bool s_Selected = false;
-        //private static bool s_Connecting = false;
 
         public static TextAsset LocalAchievementDescriptions;
 
         public static void Select()
         {
-            if (s_Selected || /*s_Connecting ||*/ Social.localUser.authenticated)
+            if (s_Selected || Social.localUser.authenticated)
             {
                 return;
             }
 
-            //s_Connecting = true;
-
             //Note: For now, on iPhone, keep the GameCenter Implementation and use our disk based implementation for anything else
             if (Application.platform != RuntimePlatform.IPhonePlayer)
             {
-                Debug.Log("[SocialConnector] Connecting Social API to LocalPlatform");
+                Debug.Log("[SocialPlatformSelector] Connecting Social API to LocalPlatform");
                 Social.Active = LocalSocialPlatform.Instance;
                 LocalSocialPlatform.Instance.LoadAchievementDescriptions(LocalAchievementDescriptions);
             }
             else
             {
-                Debug.Log("[SocialConnector] Leaving Social API connected to GameCenter");
+                Debug.Log("[SocialPlatformSelector] Leaving Social API connected to GameCenter");
             }
             //TODO: Add support for Kongregate, OpenFeint and other Social Platforms.
 
